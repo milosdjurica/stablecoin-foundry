5d118bb7a88117d20d03a403180e34cdf1c3b455
milosdjurica <milosdjurica13@gmail.com>
2023-12-19 19:12:09 +0100

    finished helperConfig and deploy script

--- script/HelperConfig.s.sol
+++ script/HelperConfig.s.sol
@@ -2,6 +2,8 @@
 pragma solidity 0.8.20;
 
 import {Script} from "lib/forge-std/src/Script.sol";
+import {MockV3Aggregator} from "../test/mocks/MockV3Aggregator.sol";
+import {ERC20Mock} from "@openzeppelin/contracts/mocks/token/ERC20Mock.sol";
 
 contract HelperConfig is Script {
     struct NetworkConfig {
@@ -12,9 +14,20 @@ contract HelperConfig is Script {
         uint256 deployerKey;
     }
 
+    uint8 public constant DECIMALS = 8;
+    int256 public constant ETH_USD_PRICE = 2000e8;
+    int256 public constant BTC_USD_PRICE = 20000e8;
+    uint256 public DEFAULT_ANVIL_KEY = 0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80;
+
     NetworkConfig public activeNetworkConfig;
 
-    constructor() {}
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliaEthConfig();
+        } else {
+            activeNetworkConfig = getOrCreateAnvilConfig();
+        }
+    }
 
     function getSepoliaEthConfig() public view returns (NetworkConfig memory) {
         return NetworkConfig({
@@ -30,7 +43,21 @@ contract HelperConfig is Script {
         if (activeNetworkConfig.wEthUsdPriceFeed != address(0)) return activeNetworkConfig;
 
         vm.startBroadcast();
+        MockV3Aggregator wethPriceFeed = new MockV3Aggregator(DECIMALS, ETH_USD_PRICE);
+
+        ERC20Mock wethMock = new ERC20Mock();
+
+        MockV3Aggregator wbtcUSDPriceFeed = new MockV3Aggregator(DECIMALS, BTC_USD_PRICE);
+        ERC20Mock wbtcMock = new ERC20Mock();
 
         vm.stopBroadcast();
+
+        return NetworkConfig({
+            wEthUsdPriceFeed: address(wethPriceFeed),
+            wBtcUsdPriceFeed: address(wbtcUSDPriceFeed),
+            wEth: address(wethMock),
+            wBtc: address(wbtcMock),
+            deployerKey: DEFAULT_ANVIL_KEY
+        });
     }
 }
