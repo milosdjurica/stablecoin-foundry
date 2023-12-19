9f03b5bb7f7bd380e288b0ac234753b679efde5b
milosdjurica <milosdjurica13@gmail.com>
2023-12-19 15:38:45 +0100

    imported openzeppelin lib and created constructor for DSC

--- src/DecentralizedStableCoin.sol
+++ src/DecentralizedStableCoin.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.20;
 
+import {ERC20Burnable, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
+
 /**
  * @title Decentralized StableCoin
  * @author Milos Djurica
@@ -10,6 +12,6 @@ pragma solidity 0.8.20;
  *
  * This is the contract meant to be governed by DSCEngine. This contract is just the ERC20
  * implementation of our stablecoin system.
- */ contract DecentralizedStableCoin {
-    constructor() {}
+ */ contract DecentralizedStableCoin is ERC20Burnable {
+    constructor() ERC20("DecentralizedStableCoin", "DSC") {}
 }
