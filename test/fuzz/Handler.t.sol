d8ec356e54466f55ea4db65dad0f9eab81233ed4
milosdjurica <milosdjurica13@gmail.com>
2023-12-20 12:45:04 +0100

    started writting handler

--- test/fuzz/Handler.t.sol
+++ test/fuzz/Handler.t.sol
@@ -2,3 +2,19 @@
 pragma solidity 0.8.20;
 
 // Handler is going to handle down the way we call function
+
+import {Test} from "lib/forge-std/src/Test.sol";
+import {DSCEngine} from "../../src/DSCEngine.sol";
+import {DecentralizedStableCoin} from "../../src/DecentralizedStableCoin.sol";
+
+contract Handler is Test {
+    DSCEngine engine;
+    DecentralizedStableCoin dsc;
+
+    constructor(DSCEngine _dscEngine, DecentralizedStableCoin _dsc) {
+        engine = _dscEngine;
+        dsc = _dsc;
+    }
+
+    // redeem collateral
+}
